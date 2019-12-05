259f4cffaad3a354b5ccf394effbbd6dfe515d35
Marios Papachristou <papachristoumarios@gmail.com>
2019-12-05 13:43:36 +0200

    Add get_argparser

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,7 +1,13 @@
 import networkx as nx
 import numpy as np
+import argparse
+import os
+from multiprocessing import cpu_count
 
 def read_graph(filename):
+    dirname, graph_type = os.path.split(filename)
+    graph_type = graph_type.strip('_all').strip('*.txt')
+    _, system = os.path.split(dirname)
 
     G = nx.DiGraph()
     with open(filename) as f:
@@ -11,4 +17,12 @@ def read_graph(filename):
         u, v = line.split()
         G.add_edge(u, v)
 
-    return G
+    return G, system, graph_type
+
+def get_argparser():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-r', help='Root directory', default='..')
+    argparser.add_argument('-w', help='Number of workers', default=cpu_count()-1)
+    argparser.add_argument('-o', help='Output file', default='../results/graph_metrics.tex')
+    argparser.add_argument('--dry-run', action='store_true')
+    return argparser
